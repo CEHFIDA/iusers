2407db6ce3f21b97cada51b9833ee8b4166aa841
Yura <fresh@mail.com>
2017-09-28 12:49:46 +0300

    add prefix for adminroles(routes)

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,9 @@
 <?php
 
-Route::group(['middleware' => 'web'], function () {
-	Route::get(config('adminamazing.path').'/users', 'selfreliance\iusers\UsersController@index')->name('AdminUsers');
-	Route::get(config('adminamazing.path').'/users/{id}', 'selfreliance\iusers\UsersController@edit')->name('AdminUsersEdit');
-	Route::put(config('adminamazing.path').'/users/{id}', 'selfreliance\iusers\UsersController@update')->name('AdminUsersUpdate');
-	Route::delete(config('adminamazing.path').'/users/{id}', 'selfreliance\iusers\UsersController@destroy')->name('AdminUsersDeleted');
-
-	Route::post(config('adminamazing.path').'/users/loginwith/{id}', 'selfreliance\iusers\UsersController@loginwith')->name('AdminUsersLoginWith');
+Route::group(['prefix' => 'admin/users', 'middleware' => 'web'], function() {
+	Route::get('/', 'selfreliance\iusers\UsersController@index')->name('AdminUsers');
+	Route::get('/{id}', 'selfreliance\iusers\UsersController@edit')->name('AdminUsersEdit');
+	Route::put('/{id}', 'selfreliance\iusers\UsersController@update')->name('AdminUsersUpdate');
+	Route::delete('/{id}', 'selfreliance\iusers\UsersController@destroy')->name('AdminUsersDeleted');
+	Route::post('/loginwith/{id}', 'selfreliance\iusers\UsersController@loginwith')->name('AdminUsersLoginWith');
 });
